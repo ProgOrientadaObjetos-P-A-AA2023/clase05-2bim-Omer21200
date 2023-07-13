dd259ac15bda867df5930e418ac43a0b73b72a56
Omer21200 <omerbenitez212000@gmail.com>
2023-07-13 11:09:26 -0500

    Apis

---
+++ EjemplosSOLID/ejemplos/EjemploSolid/EjemplosSOLID/src/paquete11/APIDisney.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete11;
+
+import java.util.Random;
+
+/**
+ *
+ * @author omerb
+ */
+public class APIDisney implements APIMovie {
+//j
+    private String apiKey;
+    private String user;
+
+    public String obtenerUser() {
+        return user;
+    }
+
+    public void establecerUser(String us) {
+        user = us;
+    }
+
+    @Override
+    public void establecerApiKey(String ak) {
+        Random rand = new Random();
+        int numeroAleatorio = rand.nextInt(900000) + 100000;
+        apiKey = ak + "DISNEY" + numeroAleatorio + user;
+    }
+
+    @Override
+    public String obtenerApiKey() {
+        return apiKey;
+    }
+
+}
---
+++ EjemplosSOLID/ejemplos/EjemploSolid/EjemplosSOLID/src/paquete11/ArchivoLectura.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete11;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ArchivoLectura {
+
+    private Scanner entrada;
+    private String nombreArchivo;
+    private String rutaArchivo;
+    private ArrayList<Aux> lista;
+
+    public ArchivoLectura(String n) {
+        nombreArchivo = n;
+        rutaArchivo = String.format("datos/%s",
+                obtenerNombreArchivo());
+
+        File f = new File(rutaArchivo);
+        if (f.exists()) {
+            try {
+                entrada = new Scanner(new File(rutaArchivo));
+                // entrada = new Scanner(f);
+            } // fin de try
+            catch (FileNotFoundException e) {
+                System.err.println("Error al leer del archivo: " + e);
+
+            } // fin de catch
+        }
+    }
+
+    public void establecerNombreArchivo(String n) {
+        nombreArchivo = n;
+    }
+
+    public void establecerRutaArchivo() {
+        rutaArchivo = String.format("datos/%s.txt",
+                obtenerNombreArchivo());;
+    }
+
+    public String obtenerNombreArchivo() {
+        return nombreArchivo;
+    }
+
+    public String obtenerRutaArchivo() {
+        return rutaArchivo;
+    }
+
+    // establecer valores de cada línea
+    // en la lista de tipo Profesor
+   public void establecerLista() {
+        lista = new ArrayList<>();
+        
+        File f = new File(rutaArchivo);
+
+        if (f.exists()) {
+
+            while (entrada.hasNext()) {
+                String linea = entrada.nextLine();
+
+                ArrayList<String> linea_partes = new ArrayList<>(
+                        Arrays.asList(linea.split(";"))
+                );
+                Aux p = new Aux(linea_partes.get(1),
+                        linea_partes.get(2));
+                lista.add(p);
+
+            } // fin de while
+        }
+    }
+
+    public ArrayList<Aux> obtenerLista() {
+
+        return lista;
+    }
+
+    public void cerrarArchivo() {
+        if (entrada != null) {
+            entrada.close();
+        } // cierra el archivo
+
+    }
+
+}
